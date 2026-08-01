7ce63e0de515a76d5ca5fd87543799643f2e7453
goldy thakur <goldykumar0807@gmail.com>
2026-08-01 20:16:37 +0530

    Add solution for 1929. Concatenation of Array
    Implemented a solution for the concatenation of an array problem on LeetCode.

---
+++ 01_arrays/1929_concatenation_of_array.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: 1929. Concatenation of Array
+Platform: LeetCode
+Difficulty: Easy
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2 * n);
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+
+        return ans;
+    }
+};
